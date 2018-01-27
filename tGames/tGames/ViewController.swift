01d90fab40135490f2c042535034d13e0c176ddb
Rodrigo Miyashiro <rodrigo.miyashiro@yahoo.com.br>
2018-01-27 02:53:51 -0200

    Started project.

---
+++ tGames/tGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tGames
+//
+//  Created by Rodrigo Miyashiro on 27/01/18.
+//  Copyright © 2018 Rodrigo Miyashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
